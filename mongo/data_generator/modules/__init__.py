71f6c42bc3b6a64c49b705d471adebf108a961b4
svvladimir-ru <svetlakov454@gmail.com>
2022-02-06 23:54:43 +0300

    убрал лишнее

--- mongo/data_generator/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .generator import Generator  # noqa: F401
-from .loader import Loader  # noqa: F401
-from .mongo import MongoManager  # noqa: F401
--- sentry-21.12/install/build-docker-images.sh
+++
@@ -1,8 +0,0 @@
-echo "${_group}Building and tagging Docker images ..."
-
-echo ""
-$dc build --force-rm
-echo ""
-echo "Docker images built."
-
-echo "${_endgroup}"
